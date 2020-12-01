05fde8e98979fe2f88c17553e630d9d4510b491b
raphael anderson <raphael.8379@hotmail.com>
2020-12-01 11:15:26 -0300

    criado arquivo index.php

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" />
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/site.webmanifest">
+<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+<title>Raphael Anderson RA</title>
+</head>
+
+<body>
+		
+       	<nav class="menu">
+	<ul>
+		<li><a href="#js">JAVASCRIPT</a></li>
+		<li><a href="#jq">JQUERY</a></li>
+		<li><a href="#css">CSS</a></li>
+		<li><a href="#php">PHP</a></li>
+        <li><a href="https://github.com/raphael8379/" target="_blank">Github</a></li>
+		
+	</ul>
+</nav> 
+
+			<h1>Bem Vindo !!</h1>
+            <h2 style="color:#990000">Navegue pelos links acima, tirando o Github que é um link externo rsrsrs !!</h2>
+            
+            <section id="js">
+         <div class="espaco">  </div>
+            <h2>JavaScript </h2>
+        <p> JavaScript (frequentemente abreviado como JS) é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma. Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias da World Wide Web. JavaScript permite páginas da Web interativas e, portanto, é uma parte essencial dos aplicativos da web. A grande maioria dos sites usa, e todos os principais navegadores têm um mecanismo JavaScript dedicado para executá-lo.  </p>
+            </section>
+            
+               <section id="jq">
+         <div class="espaco">  </div> 
+            <h2>Jquery </h2>
+        <p> jQuery é uma biblioteca de funções JavaScript que interage com o HTML, desenvolvida para simplificar os scripts interpretados no navegador do cliente (client-side). Usada por cerca de 74.4% dos 10 mil sites mais visitados do mundo, jQuery é a mais popular das bibliotecas JavaScript. Em agosto de 2005, ao observar o comportamento da biblioteca JavaScript Behaviour.js o criador da biblioteca jQuery John Resig começou a criar a sintaxe do jQuery.  </p>
+            </section>
+            
+               <section id="css">  
+              <div class="espaco">  </div>     
+            <h2>CSS </h2>
+        <p> Cascading Style Sheets (CSS) é um mecanismo para adicionar estilo (cores, fontes, espaçamento, etc.) a um documento web.
+        O código CSS pode ser aplicado diretamente nas tags ou ficar contido dentro das tags STYLE. Também é possível, em vez de colocar a formatação dentro do documento, criar um link para um arquivo CSS que contém os estilos.
+          </p>
+            </section>
+            
+            
+               <section id="php">  
+            <div class="espaco">  </div> 
+            <h2>PHP </h2>
+        <p> PHP (um acrônimo recursivo para "PHP: Hypertext Preprocessor", originalmente Personal Home Page) é uma linguagem interpretada livre, usada originalmente apenas para o desenvolvimento de aplicações presentes e atuantes no lado do servidor, capazes de gerar conteúdo dinâmico na World Wide Web. Figura entre as primeiras linguagens passíveis de inserção em documentos HTML, dispensando em muitos casos o uso de arquivos externos para eventuais processamentos de dados. O código é interpretado no lado do servidor pelo módulo PHP, que também gera a página web a ser visualizada no lado do cliente. </p>
+            </section>
+            
+            
+
+ <script src="js/scripts.js"></script>
+</body>
+</html>
